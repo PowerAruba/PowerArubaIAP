6eb5569ba206a51829022274e430b807dcb2c5d4
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-05-13 14:07:12 +0200

    RestMethod: Add Invoke-ArubaIAPRestMethod for call REST Aruba IAP API

---
+++ PowerArubaIAP/Private/RestMethod.ps1
@@ -0,0 +1,76 @@
+#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Invoke-ArubaIAPRestMethod {
+
+    <#
+      .SYNOPSIS
+      Invoke RestMethod with ArubaIAP connection (internal) variable
+
+      .DESCRIPTION
+       Invoke RestMethod with ArubaIAP connection variable (token, csrf..)
+
+      .EXAMPLE
+      Invoke-ArubaIAPRestMethod -method "get" -uri "rest/virtual-controller-ip"
+
+      Invoke-RestMethod with ArubaIAP connection for get rest/v1/rest/virtual-controller-ip
+
+      .EXAMPLE
+      Invoke-ArubaIAPRestMethod "rest/v1/rest/virtual-controller-ip"
+
+      Invoke-RestMethod with ArubaIAP connection for get rest/v1/rest/virtual-controller-ip uri with default GET method parameter
+
+      .EXAMPLE
+      Invoke-ArubaIAPRestMethod -method "post" -uri "rest/v1/rest/virtual-controller-ip" -body $body
+
+      Invoke-RestMethod with ArubaIAP connection for post rest/v1/rest/virtual-controller-ip uri with $body payload
+ers
+    #>
+
+    [CmdletBinding(DefaultParametersetname="default")]
+    Param(
+        [Parameter(Mandatory = $true, position = 1)]
+        [String]$uri,
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("GET", "PUT", "POST", "DELETE")]
+        [String]$method = "get",
+        [Parameter(Mandatory = $false)]
+        [psobject]$body
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $Server = ${DefaultArubaIAPConnection}.Server
+        $headers = ${DefaultArubaIAPConnection}.headers
+        $invokeParams = ${DefaultArubaIAPConnection}.invokeParams
+
+        $fullurl = "https://${Server}/${uri}"
+        if($fullurl -NotMatch "\?"){
+            $fullurl += "?"
+        }
+
+        $sessionvariable = $DefaultArubaIAPConnection.session
+        try {
+            if ($body) {
+                $response = Invoke-RestMethod $fullurl -Method $method -body ($body | ConvertTo-Json) -Headers $headers -WebSession $sessionvariable @invokeParams
+            }
+            else {
+                $response = Invoke-RestMethod $fullurl -Method $method -Headers $headers -WebSession $sessionvariable @invokeParams
+            }
+        }
+
+        catch {
+            Show-ArubaIAPException $_
+            throw "Unable to use ArubaIAP API"
+        }
+        $response
+
+    }
+
+}
