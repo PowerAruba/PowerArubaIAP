5c2eea3c940ac9ad6670fb7d091aa7a5fb21c49a
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-11-11 07:55:16 +0100

    hostname: Add Set-ArubaIAPHostname for set IAP Name

---
+++ PowerArubaIAP/Public/Action/hostname.ps1
@@ -0,0 +1,58 @@
+#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Set-ArubaIAPHostname {
+
+    <#
+        .SYNOPSIS
+        Set the name of Aruba Instant AP
+
+        .DESCRIPTION
+        Configure hostname of Aruba Instant AP
+
+        .EXAMPLE
+        Set-ArubaIAPHostname PowerArubaIAP-AP1
+
+        Set PowerArubaIAP-AP1 hostname on IAP
+
+        .EXAMPLE
+        Set-ArubaIAPHostname -hostname PowerArubaIAP-AP2 -iap_ip_addr 192.0.2.2
+
+        Set PowerArubaIAP-AP2 hostname on IAP with address IP 192.0.2.2
+    #>
+
+    Param(
+        [Parameter (Mandatory = $true, Position = 1)]
+        [string]$hostname,
+        [Parameter (Mandatory = $false)]
+        [ipaddress]$iap_ip_addr = ${DefaultArubaIAPConnection}.iap_ip_addr
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $uri = "rest/hostname"
+
+
+        $hostname_info = @{
+            "hostname" = $hostname
+        }
+
+        $body = @{
+            "iap_ip_addr"   = $iap_ip_addr.ToString()
+            "hostname_info" = $hostname_info
+        }
+
+        $response = Invoke-ArubaIAPRestMethod -uri $uri -body $body -method 'POST'
+
+        $response
+    }
+
+    End {
+    }
+}
