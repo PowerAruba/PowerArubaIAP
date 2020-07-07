f3d53831a0a334da75d95ec7e7acdb0e64f6bf9b
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-07-07 22:30:30 +0200

    SyslogServer(Configuration): Set and Remove Syslog Server (IPAddress)

--- PowerArubaIAP/Public/Configuration/syslog.ps1
+++ PowerArubaIAP/Public/Configuration/syslog.ps1
@@ -119,3 +119,110 @@ function Remove-ArubaIAPSyslogLevel {
     End {
     }
 }
+
+function Set-ArubaIAPSyslogServer {
+
+    <#
+        .SYNOPSIS
+        Set a Syslog Server to Aruba Instant AP
+
+        .DESCRIPTION
+        Set Server syslog (IP Address or FQDN) to Aruba Instant AP
+
+        .EXAMPLE
+        Set-ArubaIAPSyslogServer 192.0.2.1
+
+        Set Syslog Server 192.0.2.1 to IAP
+
+        .EXAMPLE
+        Set-ArubaIAPSyslogServer -syslog "192.0.2.1 192.0.2.2"
+
+        Set Syslog Server 192.0.2.1 and 192.0.2.2 to IAP
+    #>
+
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Low')]
+    Param(
+        [Parameter (Mandatory = $true, Position = 1)]
+        [string]$syslog,
+        [Parameter (Mandatory = $false)]
+        [ipaddress]$iap_ip_addr = ${DefaultArubaIAPConnection}.iap_ip_addr
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $uri = "rest/syslog-server"
+
+        $syslog_server = @{
+            "action"           = "create"
+            "syslog_server_ip" = $syslog
+        }
+
+        $body = @{
+            "syslog-server" = $syslog_server
+        }
+
+        if ($PSCmdlet.ShouldProcess($iap_ip_addr, 'Set Syslog Server')) {
+            $response = Invoke-ArubaIAPRestMethod -uri $uri -body $body -method 'POST'
+
+            $response
+        }
+    }
+
+    End {
+    }
+}
+
+function Remove-ArubaIAPSyslogServer {
+
+    <#
+        .SYNOPSIS
+        Remove Syslog Server to Aruba Instant AP
+
+        .DESCRIPTION
+        Remove Syslog Server to Aruba Instant AP
+
+        .EXAMPLE
+        Remove-ArubaIAPSyslogServer
+
+        Remove Syslog Server to IAP
+
+        .EXAMPLE
+        Remove-ArubaIAPSyslogServer -confirm:$false
+
+        Remove Syslog Server to IAP without confirmation
+    #>
+
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'high')]
+    Param(
+        [Parameter (Mandatory = $false)]
+        [ipaddress]$iap_ip_addr = ${DefaultArubaIAPConnection}.iap_ip_addr
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $uri = "rest/syslog-server"
+
+        $syslog_server = @{
+            "action" = "delete"
+        }
+
+        $body = @{
+            "syslog-server" = $syslog_server
+        }
+
+        if ($PSCmdlet.ShouldProcess($iap_ip_addr, 'Remove Syslog Server')) {
+            $response = Invoke-ArubaIAPRestMethod -uri $uri -body $body -method 'POST'
+
+            $response
+        }
+    }
+
+    End {
+    }
+}
